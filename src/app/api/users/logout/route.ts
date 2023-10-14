db297b6bed525036d109420a9db6040a408a1e48
jeffmichaelgomez <jeffmichaelgomez@gmail.com>
2023-10-15 03:29:05 +0800

    chore: remove isAdmin token

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -7,7 +7,6 @@ export async function GET(){
             success: true,
         })
         response.cookies.set("token", "", {httpOnly: true, expires: new Date(0)})
-        response.cookies.set("isAdmin", "", {httpOnly: true, expires: new Date(0)})
         return response;
     }catch(error: any){
         return NextResponse.json({ error: error.message}, {status: 500});
